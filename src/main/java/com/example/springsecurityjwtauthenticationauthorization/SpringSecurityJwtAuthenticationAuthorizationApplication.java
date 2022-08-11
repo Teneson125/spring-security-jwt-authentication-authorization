dec9aa375c8f33f7d42e70339dfafb108f8a6827
Manoj <kannamanoj2@gmail.com>
2022-08-11 20:46:25 +0530

    Initial commit

---
+++ src/main/java/com/example/springsecurityjwtauthenticationauthorization/SpringSecurityJwtAuthenticationAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwtauthenticationauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthenticationAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtAuthenticationAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityjwtauthenticationauthorization/SpringSecurityJwtAuthenticationAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwtauthenticationauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAuthenticationAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
